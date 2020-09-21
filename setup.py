9f75d08da099fd1ea9529a20cdb8cddc2631e6f8
xikasan <xika@xika.jp>
2020-09-21 13:57:14 +0900

    update minor version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('LISENCE') as f:
 
 setup(
     name='xair',
-    version='0.3.4',
+    version='0.3.5',
     description="xikasan's aircraft simulation tool set",
     long_description=readme,
     author='xikasan',
